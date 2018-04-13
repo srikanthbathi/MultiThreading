ae940dfc8fc51418f3b38053cedf51524d89f746
Srikanth Bathi <bathi.srikanth@sap.com>
2018-04-13 14:37:39 +0530

    2
    Change-Id: Id7fe948d01365d923ad11b0574bb1bded6c67700
    Signed-off-by: Srikanth Bathi <bathi.srikanth@sap.com>

--- MultiThreading/src/basics/Client.java
+++ MultiThreading/src/basics/Client.java
@@ -1,10 +1,32 @@
 package basics;
+// When we run the following java code in debug mode and normal mode, we get two different outputs.
+//Program running in normal mode outputs as follow. 
+/*
+ main method runs
+Thread running
+ */   //because,  At a time one thread is executed only. So now the current thread is running the main method, the fist sysout will be 
+//main method runs and then "Thread running"
 
-public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+// try putting the Thread.sleep(2000); after start() method then the output order will be different.
+//this is because the current thread is idle for 2000ms, the cpu gets allocated other thread we created.
 
+
+
+public class Client extends Thread{
+	
+	@Override
+	public void run(){
+		System.out.println("Thread running");
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		Client c = new Client();
+		
+        c.start();
+        Thread.sleep(2000);
+        System.out.println("main method runs");
 	}
 
 }
